2fc8269c3c31d25735ff9558a142c8fa27d78a27
Eli Bork <ebork@usc.edu>
2018-09-29 14:46:26 -0700

    Added DropPowerup script
    makes it so enemies have a chance of dropping powerups at death. Powerups are stored in an array that can be modified in editor. Also, drop rate can be modified in editor.

---
+++ NEW Newbies and Vets Game/Assets/Scripts/DropPowerup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DropPowerup : MonoBehaviour {
+
+	
+	public GameObject[] powerups;
+	public int enemyHealth = 100;
+	public float chanceToDropPowerup = 5f; //the percent chance that any given enemy will drop a powerup on death
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		
+	if(enemyHealth <= 0){
+
+		int whichPowerUp = Random.Range(0, powerups.Length + 1);
+		float rand = Random.Range(1f, 101f);
+
+		if(rand <= chanceToDropPowerup){
+			Instantiate(powerups[whichPowerUp], this.transform.position, this.transform.rotation);
+		}
+
+		this.enabled = false;
+	}
+	
+	}
+}
