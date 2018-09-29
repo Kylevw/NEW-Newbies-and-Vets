ff66efbdf4123cc044c312f8b5775e4eeb74ade4
Kylevw <vanwilte@usc.edu>
2018-09-29 14:20:36 -0700

    First Upload

---
+++ NEW Newbies and Vets Game/Assets/Scripts/SampleMovementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleMovementScript : MonoBehaviour {
+
+    public float speed;
+    public float rotationSpeed;
+    Vector3 currentMovement;
+	
+	// Update is called once per frame
+	void Update () {
+
+        currentMovement.x += (Random.value - 0.5f) * (rotationSpeed);
+        currentMovement.y += (Random.value - 0.5f) * (rotationSpeed);
+        currentMovement = currentMovement.normalized * speed;
+
+        transform.position += currentMovement * Time.deltaTime;
+	}
+}
