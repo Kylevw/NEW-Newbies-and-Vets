358a2f7ff4f7c20908715bf4d3c05b99b8297cfa
carterblock <carterpblock@gmail.com>
2018-09-29 15:56:25 -0700

    Worked on mixing the DS4 controls with the player movement scripts

--- NEW Newbies and Vets Game/Assets/Scripts/PlayerMovement.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : PhysicsObject {
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 15;
+    public float playerID;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,8 +15,18 @@ public class PlayerMovement : PhysicsObject {
     protected override void ComputeVelocity()
     {
         Vector2 move = Vector2.zero;
-        move.x = Input.GetAxis("Horizontal");
-        if(Input.GetButtonDown("Jump") && grounded)
+        move.x = Input.GetAxis("P" + playerID +"_Move");
+        if(move.x < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(move.x > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+
+        if(Input.GetButtonDown("P" + playerID +"_Jump") && grounded)
         {
             velocity.y = jumpTakeOffSpeed;
         }
