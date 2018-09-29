c0f782d503f34ceef3f5771af48c07d3f93addc1
Carter Block <carterpblock@gmail.com>
2018-09-29 15:05:17 -0700

    Add files via upload

---
+++ NEW Newbies and Vets Game/Assets/Scripts/P1_Joystick_Aiming.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1_Joystick_Aiming : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("P1_AimX");
+        float inputY = Input.GetAxis("P1_AimY");
+
+        if (inputX != 0 || inputY != 0)
+        {
+            Vector2 aimDirection = new Vector2(inputX, inputY);
+
+            Debug.Log(aimDirection);
+        }
+	}
+}
---
+++ NEW Newbies and Vets Game/Assets/Scripts/P2_Joystick_Aiming.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2_Joystick_Aiming : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inputX = Input.GetAxis("P2_AimX");
+        float inputY = Input.GetAxis("P2_AimY");
+
+        if (inputX != 0 || inputY != 0)
+        {
+            Vector2 aimDirection = new Vector2(inputX, inputY);
+
+            Debug.Log(aimDirection);
+        }
+
+    }
+}
