e1f2264b2eded64990b4d37c6627e235ca1fce2b
isaacrtang99 <irtang@usc.edu>
2018-09-30 14:08:18 -0700

    Spawning rework

--- NEW Newbies and Vets Game/Assets/Scripts/SwoopingMonsterSpawner.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/SwoopingMonsterSpawner.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwoopingMonsterSpawner : Spawner {
+public class SwoopingMonsterSpawner : MonoBehaviour
+{
 
-    public GameObject[] SwoopPrefab;
+    public GameObject[] SwoopingPrefab;
     public float spawnTime;
     public float timeTilSpawn;
-    
-    public override void Spawn()
+    void Update()
     {
-        Instantiate(SwoopPrefab[(int)(SwoopPrefab.Length * Random.value)], transform.position, Quaternion.identity);
+        if (timeTilSpawn <= 0.0f)
+        {
+            Instantiate(SwoopingPrefab[(int)(SwoopingPrefab.Length * Random.value)], transform.position, Quaternion.identity);
+            timeTilSpawn = spawnTime;
+            spawnTime = Random.value * 4 + 4;
+        }
+        timeTilSpawn -= Time.deltaTime;
     }
 }
