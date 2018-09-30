479060ebd0133d0400f4fb4eb48da154781381ca
isaacrtang99 <irtang@usc.edu>
2018-09-29 21:19:52 -0700

    Stay Away Distance and rayhit removal
    Set an area for flying enemies to fly to

--- NEW Newbies and Vets Game/Assets/Scripts/GroundMonsterMovement.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/GroundMonsterMovement.cs
@@ -8,17 +8,13 @@ public class GroundMonsterMovement : PhysicsObject {
     public float timeTilChange;
     public float currentTime;
     protected Vector2 move = Vector2.zero;
-    public float spriteWidth = .16f;
-    public float spriteHeight = .16f;
-    public bool check;
-    public RaycastHit2D edgeCheckLeft;
-    public RaycastHit2D edgeCheckRight;
+    
+    
     public GameObject[] players;
     void Start () {
         currentTime = timeTilChange;
         players = GameObject.FindGameObjectsWithTag("Player");
-        edgeCheckLeft = Physics2D.Raycast(transform.position +  new Vector3(-spriteWidth/2,-spriteHeight/2,0), Vector2.down,1);
-        edgeCheckLeft = Physics2D.Raycast(transform.position +  new Vector3(spriteWidth / 2, -spriteHeight / 2, 0), Vector2.down, 1);
+        
         if (players.Length == 2)
         {
             if (Vector2.Distance(transform.position, players[0].transform.position) <= Vector2.Distance(transform.position, players[1].transform.position))
@@ -54,8 +50,7 @@ public class GroundMonsterMovement : PhysicsObject {
         {
             player = players[0].transform;
         }
-        check = edgeCheckLeft.collider == null;
-        if (edgeCheckLeft.collider != null || edgeCheckRight.collider !=null) {
+        
             if (Mathf.Abs(player.position.x - transform.position.x) <= 5 && Mathf.Abs(player.position.x - transform.position.x) > .8 )
             {
                 if (player.position.x > transform.position.x)
@@ -95,14 +90,8 @@ public class GroundMonsterMovement : PhysicsObject {
                 move = Vector2.zero;
                 targetVelocity = move * maxSpeed;
             }
-        }
-        else
-        {
-            move = Vector2.zero;
-            targetVelocity = move * maxSpeed;
-        }
-        Debug.DrawRay(transform.position + new Vector3(-spriteWidth / 2, -spriteHeight / 2, 0), Vector2.down, Color.red);
-        Debug.DrawRay(transform.position + new Vector3(spriteWidth / 2, -spriteHeight / 2, 0), Vector2.down, Color.red);
+        
+       
 
 
 
