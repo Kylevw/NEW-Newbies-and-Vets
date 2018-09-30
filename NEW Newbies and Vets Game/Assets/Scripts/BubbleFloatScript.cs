1c6d6dd010849ed1457eed7dad648d5d1932ddea
Kylevw <vanwilte@usc.edu>
2018-09-29 21:19:38 -0700

    Fixed Animators

---
+++ NEW Newbies and Vets Game/Assets/Scripts/BubbleFloatScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleFloatScript : MonoBehaviour {
+
+    public float speed;
+    public float displacement;
+    float aliveTime;
+
+    SpriteRenderer sprite;
+    Color defaultColor;
+
+    public Color pulseColor;
+    public float pulseSpeed;
+    float pulseTime;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+        defaultColor = sprite.color;
+    }
+
+    void Update () {
+
+        transform.position += Vector3.up * Mathf.Sin(aliveTime) * displacement;
+        aliveTime += Time.deltaTime * speed;
+
+        sprite.color = Color.Lerp(defaultColor, pulseColor, Mathf.PingPong(pulseTime, 1));
+        Debug.Log(Mathf.PingPong(pulseTime, 1));
+        pulseTime += Time.deltaTime * pulseSpeed;
+
+	}
+}
