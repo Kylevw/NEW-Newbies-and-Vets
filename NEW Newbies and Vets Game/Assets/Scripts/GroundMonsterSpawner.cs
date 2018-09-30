ce95e305bd82734024a2d15a8629c0ea2dee1569
isaacrtang99 <irtang@usc.edu>
2018-09-29 20:07:05 -0700

    Time update
    Updated the way time is checked

--- NEW Newbies and Vets Game/Assets/Scripts/GroundMonsterSpawner.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/GroundMonsterSpawner.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundMonsterSpawner : MonoBehaviour {
+public class GroundMonsterSpawner : MonoBehaviour
+{
+    public GameObject[] GroundPrefab;
+    public float spawnTime;
+    public float timeTilSpawn;
+    public float wave;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        timeTilSpawn = 0.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeTilSpawn <= 0.0f)
+        {
+
+            Instantiate(GroundPrefab[(int)(GroundPrefab.Length * Random.value)], transform.position, Quaternion.identity);
+            timeTilSpawn = spawnTime;
+        }
+        timeTilSpawn -= Time.deltaTime;
+    }
 }
