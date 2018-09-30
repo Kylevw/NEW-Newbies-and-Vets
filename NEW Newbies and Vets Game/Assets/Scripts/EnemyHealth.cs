4015062a6a219d8a543159f39058db4a55d01554
Eli Bork <ebork@usc.edu>
2018-09-30 11:01:22 -0700

    test commit

---
+++ NEW Newbies and Vets Game/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int enemyHealth = 100;
+    public int damage = 25;
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "player projectile")
+        {
+            enemyHealth -= damage;
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+        if(enemyHealth <= 0)
+        {
+            
+        }
+
+
+	}
+}
