6c0336f45122c82c84e1af960d7771b9db28b2b5
Kylevw <vanwilte@usc.edu>
2018-09-29 22:16:31 -0700

    Power-Up Animations & Despawn

--- NEW Newbies and Vets Game/Assets/Scripts/BubbleFloatScript.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/BubbleFloatScript.cs
@@ -15,19 +15,42 @@ public class BubbleFloatScript : MonoBehaviour {
     public float pulseSpeed;
     float pulseTime;
 
+    public float spawnTime;
+    public float aliveTimeMin;
+    public float aliveTimeMax;
+
+    float timeToDespawn;
+
     void Start() {
         sprite = GetComponent<SpriteRenderer>();
         defaultColor = sprite.color;
+
+        timeToDespawn = (Random.value * (aliveTimeMax - aliveTimeMin)) + aliveTimeMin;
     }
 
     void Update () {
 
-        transform.position += Vector3.up * Mathf.Sin(aliveTime) * displacement;
-        aliveTime += Time.deltaTime * speed;
+
+
+        transform.position += Vector3.up * Mathf.Sin(aliveTime * speed) * displacement;
+        aliveTime += Time.deltaTime;
 
         sprite.color = Color.Lerp(defaultColor, pulseColor, Mathf.PingPong(pulseTime, 1));
         Debug.Log(Mathf.PingPong(pulseTime, 1));
         pulseTime += Time.deltaTime * pulseSpeed;
+
+        if (aliveTime >= timeToDespawn) {
+            
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, transform.localScale.x * spawnTime);
+            if (transform.localScale.x < 0.1f) {
+                Destroy(gameObject);
+            }
+
+        } else {
+            
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(1, 1, 1), (1 - transform.localScale.x) * spawnTime);
+
+        }
 
 	}
 }
