d306a0bdc1c6b380ee380fec032fe204fa5a8061
Eli Bork <ebork@usc.edu>
2018-09-30 11:13:44 -0700

    enemy health is added

--- NEW Newbies and Vets Game/Assets/Scripts/EnemyHealth.cs
+++ NEW Newbies and Vets Game/Assets/Scripts/EnemyHealth.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour {
 
+
+    //this still needs to play death sound!
+
     public int enemyHealth = 100;
     public int damage = 25;
+    
 
-     void OnCollisionEnter2D(Collision2D collision)
+    public AudioClip dieSound;
+
+  
+   
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "player projectile")
         {
             enemyHealth -= damage;
             Destroy(collision.gameObject);
+            
+
         }
     }
 
     // Update is called once per frame
     void Update () {
-		
-        if(enemyHealth <= 0)
+
+        if (enemyHealth <= 0)
         {
-            
+            Destroy(gameObject);
+
         }
-
-
 	}
 }
