762587fb8b319b872d949040e70cc0f6a4e7436a
Eli Bork <ebork@usc.edu>
2018-09-29 19:15:39 -0700

    Added PowerUp.cs and PlayerProperties.cs
    PowerUp handles the effects of different permanent power ups.
    
    player properties manages the base stats of both players (max health, base damage, ect)

---
+++ NEW Newbies and Vets Game/Assets/Scripts/PowerUp/PlayerProperties.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour {
+
+    /*
+     * This script controls all player properties like health, speed, jump height, ect.
+     * It is modular so you can add what you need to it.
+     * To use these variables, make sure you pass them to your script!
+     * 
+     * these values will be modified by powerups via the powerUp script
+     */
+
+    public int health;
+    public int moveSpeed;
+    public int attack;
+    public int jumpHeight;
+    public float resTime;
+
+    
+    
+
+}
