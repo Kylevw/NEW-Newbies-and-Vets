a7fe01a7fdd8dc319eb18412df5c66dbc78a1ec6
Eli Bork <ebork@usc.edu>
2018-09-30 17:45:44 -0700

    change

---
+++ NEW Newbies and Vets Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private int playerID1 = 1;
+    private int playerID2 = 2;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("P" + playerID1 + "_Interact") || Input.GetButtonDown("P" + playerID2 + "_Interact"))
+        {
+            Debug.Log("Next scene loaded");
+            SceneManager.LoadScene(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Next scene loaded");
+            SceneManager.LoadScene(1);
+        }
+    }
+}
